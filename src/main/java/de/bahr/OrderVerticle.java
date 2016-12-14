ff37d6f5cec649611ae0d7e6b732293b7d3c15e2
Marek Walasek <mawalasek@googlemail.com>
2016-12-14 15:07:18 +0100

    Implemented endpoint for monthly value + test

--- src/main/java/de/bahr/OrderVerticle.java
+++ src/main/java/de/bahr/OrderVerticle.java
@@ -1,43 +1,118 @@
 package de.bahr;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
+import io.vertx.core.Handler;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.mongo.MongoClient;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 public class OrderVerticle extends AbstractVerticle {
 
+    private MongoClient mongoClient;
     private Logger logger;
+    private Router router;
 
     @Override
     public void start(Future<Void> future) {
 
+        initLogger();
+        initMongoClient();
+        initRouter();
+        initHttpServer(future);
+    }
+
+    private void initLogger() {
         BasicConfigurator.configure();
         logger = Logger.getLogger(this.getClass().getName());
+    }
 
+    private void initMongoClient() {
         logger.debug("Initialising Mongo client. Config: " + config());
-        MongoClient mongoClient = MongoClient.createShared(vertx, config());
+        mongoClient = MongoClient.createShared(vertx, config());
+    }
 
-        Router router = Router.router(vertx);
+    private void initRouter() {
+        router = Router.router(vertx);
+        router.get("/orders").handler(this::handleGetAllOrders);
+        router.get("/orders/month/:year/:month/:client").handler(this::handleGetMonthlyAggregatePrice);
+    }
 
-        router.route("/orders").handler(routingContext -> {
-            HttpServerResponse response = routingContext.response();
-            response.exceptionHandler(routingContext::fail);
+    private void handleGetAllOrders(RoutingContext routingContext) {
+        HttpServerResponse response = routingContext.response();
+        response.exceptionHandler(routingContext::fail);
 
-            mongoClient.find("order", new JsonObject(), results -> {
-                    List<JsonObject> jsonOrders = results.result();
-                    response.putHeader("content-type", "application/json")
-                            .end(Json.encodePrettily(jsonOrders));
-                });
-            });
+        mongoClient.find("order", new JsonObject(), results -> {
+            List<JsonObject> jsonOrders = results.result();
+            response.putHeader("content-type", "application/json")
+                    .end(Json.encodePrettily(jsonOrders));
+        });
+    }
 
+    private void handleGetMonthlyAggregatePrice(RoutingContext routingContext) {
+        String client = routingContext.request().getParam("client");
+        int year = Integer.valueOf(routingContext.request().getParam("year"));
+        int month = Integer.valueOf(routingContext.request().getParam("month"));
+
+        HttpServerResponse response = routingContext.response();
+        response.exceptionHandler(routingContext::fail);
+
+        getMonthlyAggregatePriceFromDb(client, year, month, as -> {
+            response.putHeader("content-type", "application/json")
+                    .end(Json.encodePrettily(as.result()));
+        });
+    }
+
+    private void getMonthlyAggregatePriceFromDb(String client, int year, int month, Handler<AsyncResult<Integer>> handler) {
+        JsonObject query = new JsonObject()
+                .put("client", client)
+                .put("status", "contracted")
+                .put("expectedPrice", new JsonObject().put("$gt", 0))
+                .put("completed", new JsonObject().put("$exists", true));
+
+        mongoClient.find("order", query, results -> {
+            List<JsonObject> orders = results.result();
+            int sum = orders.stream()
+                    .filter(order -> orderCompletedDateMatches(order, year, month))
+                    .mapToInt(this::getOrderExpectedPrice)
+                    .sum();
+            handler.handle(Future.succeededFuture(sum));
+        });
+    }
+
+    private boolean orderCompletedDateMatches(JsonObject order, int year, int month) {
+        String dateStr = order.getJsonObject("completed").getString("$date");
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar cal = new GregorianCalendar();
+        try {
+            Date date = df.parse(dateStr);
+            cal.setTime(date);
+            return cal.get(Calendar.YEAR) == year && cal.get(Calendar.MONTH) +1 == month;
+        } catch (ParseException e) {
+            logger.error("Could not parse date string: " + dateStr);
+            return false;
+        }
+    }
+
+    private int getOrderExpectedPrice(JsonObject order) {
+        return order.getInteger("expectedPrice");
+    }
+
+    private void initHttpServer(Future<Void> future) {
         vertx.createHttpServer()
                 .requestHandler(router::accept)
                 .listen(config().getInteger("http.port", 8080), result -> {
--- src/test/java/de/bahr/OrderVerticleTest.java
+++ src/test/java/de/bahr/OrderVerticleTest.java
@@ -8,9 +8,10 @@ import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
 import de.flapdoodle.embed.mongo.config.Net;
 import de.flapdoodle.embed.mongo.distribution.Version;
 import de.flapdoodle.embed.process.runtime.Network;
-import io.vertx.core.DeploymentOptions;
-import io.vertx.core.Vertx;
+import io.vertx.core.*;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.mongo.MongoClient;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
@@ -20,6 +21,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RunWith(VertxUnitRunner.class)
 public class OrderVerticleTest {
@@ -27,17 +31,25 @@ public class OrderVerticleTest {
     public static final int MONGO_PORT = 12345;
     public static final int VERTICLE_PORT = 8081;
     public static final String MONGO_DB_NAME = "eve";
+    private static final String ORDERS = "order";
 
     private Vertx vertx;
     private static MongodProcess mongodProcess;
+    private MongoClient mongoClient;
 
     @Before
     public void setUp(TestContext testContext) throws Exception {
-        setupMongo();
+        setupVertx();
+        setupInMemoryMongoService();
+        setupMongoClient();
         setupVerticle(testContext);
     }
 
-    private void setupMongo() throws IOException {
+    private void setupVertx() {
+        vertx = Vertx.vertx();
+    }
+
+    private void setupInMemoryMongoService() throws IOException {
         MongodStarter mongodStarter = MongodStarter.getDefaultInstance();
         IMongodConfig mongodConfig = new MongodConfigBuilder()
                 .version(Version.Main.PRODUCTION)
@@ -47,16 +59,20 @@ public class OrderVerticleTest {
         mongodProcess = mongodExecutable.start();
     }
 
-    private void setupVerticle(TestContext testContext) {
-        vertx = Vertx.vertx();
+    private void setupMongoClient() {
+        mongoClient = MongoClient.createNonShared(vertx, testConfig());
+    }
 
-        JsonObject testConfig = new JsonObject()
+    private void setupVerticle(TestContext testContext) {
+        DeploymentOptions deploymentOptions = new DeploymentOptions().setConfig(testConfig());
+        vertx.deployVerticle(OrderVerticle.class.getName(), deploymentOptions, testContext.asyncAssertSuccess());
+    }
+
+    private JsonObject testConfig() {
+        return new JsonObject()
                 .put("http.port", VERTICLE_PORT)
                 .put("db_name", MONGO_DB_NAME)
                 .put("connection_string", "mongodb://localhost:" + MONGO_PORT);
-        DeploymentOptions deploymentOptions = new DeploymentOptions().setConfig(testConfig);
-
-        vertx.deployVerticle(OrderVerticle.class.getName(), deploymentOptions, testContext.asyncAssertSuccess());
     }
 
     @After
@@ -66,16 +82,129 @@ public class OrderVerticleTest {
     }
 
     @Test
-    public void testOrderVerticle(TestContext testContext) throws Exception {
+    public void testGetAllOrders_empty(TestContext testContext) throws Exception {
         final Async async = testContext.async();
 
         vertx.createHttpClient().getNow(VERTICLE_PORT, "localhost", "/orders", response -> {
                     testContext.assertTrue(response.statusCode() == 200);
                     response.bodyHandler(body -> {
                         testContext.assertTrue(body != null);
+                        testContext.assertEquals(body.toJsonArray().size(), 0);
                     });
                     async.complete();
                 }
+        );
+    }
+
+    @Test
+    public void testGetAllOrders_threeEntries(TestContext testContext) {
+        final Async async = testContext.async();
+
+        JsonObject order1 = new JsonObject()
+                .put("client", "John Doe")
+                .put("items", new JsonArray()
+                        .add(new JsonObject()
+                            .put("name", "Item1"))
+                        .add(new JsonObject()
+                            .put("name", "Item2")));
+
+        JsonObject order2 = new JsonObject()
+                .put("client", "Foo Bar")
+                .put("items", new JsonArray()
+                        .add(new JsonObject()
+                                .put("name", "Item3"))
+                        .add(new JsonObject()
+                                .put("name", "Item4"))
+                        .add(new JsonObject()
+                                .put("name", "Item5")));
+
+        JsonObject order3 = new JsonObject()
+                .put("client", "Hello World")
+                .put("items", new JsonArray());
+
+        Future<String> future1 = Future.future();
+        Future<String> future2 = Future.future();
+        Future<String> future3 = Future.future();
+
+        mongoClient.insert(ORDERS, order1, future1.completer())
+            .insert(ORDERS, order2, future2.completer())
+            .insert(ORDERS, order3, future3.completer());
+
+        CompositeFuture.all(future1, future2, future3).setHandler(event -> {
+            vertx.createHttpClient().getNow(VERTICLE_PORT, "localhost", "/orders", response -> {
+                        testContext.assertTrue(response.statusCode() == 200);
+                        response.bodyHandler(body -> {
+                            testContext.assertEquals(body.toJsonArray().size(), 3);
+                        });
+                        async.complete();
+                    }
+            );
+        });
+    }
+
+    @Test
+    public void testGetMonthlyValue(TestContext testContext) {
+        final Async async = testContext.async();
+
+        Map<JsonObject, Future> ordersAndFutures = new HashMap<>();
+
+        ordersAndFutures.put(new JsonObject()
+                .put("client", "John Doe")
+                .put("status", "contracted")
+                .put("expectedPrice", 123)
+                .put("completed", new JsonObject().put("$date", "2016-08-30T23:59:59.999Z")),
+                Future.future());
+
+        ordersAndFutures.put(new JsonObject()
+                .put("client", "John Doe")
+                .put("status", "contracted")
+                .put("expectedPrice", 456)
+                .put("completed", new JsonObject().put("$date", "2016-08-01T00:00:00.000Z")),
+                Future.future());
+
+        ordersAndFutures.put(new JsonObject()
+                .put("client", "John Doe")
+                .put("status", "contracted")
+                .put("expectedPrice", 789)
+                .put("completed", new JsonObject().put("$date", "2016-07-31T23:59:59.999Z")),
+                Future.future());
+
+        ordersAndFutures.put(new JsonObject()
+                .put("client", "John Doe")
+                .put("status", "contracted")
+                .put("expectedPrice", 456),
+                Future.future());
+
+        ordersAndFutures.put(new JsonObject()
+                .put("client", "Alice Doe")
+                .put("status", "contracted")
+                .put("expectedPrice", 789)
+                .put("completed", new JsonObject().put("$date", "2016-08-15T23:59:59.999Z")),
+                Future.future());
+
+        ordersAndFutures.put(new JsonObject()
+                .put("client", "John Doe")
+                .put("status", "not_contracted")
+                .put("expectedPrice", 789)
+                .put("completed", new JsonObject().put("$date", "2016-08-15T23:59:59.999Z")),
+                Future.future());
+
+        ordersAndFutures.put(new JsonObject()
+                .put("client", "John Doe")
+                .put("status", "contracted")
+                .put("expectedPrice", 0)
+                .put("completed", new JsonObject().put("$date", "2016-08-15T23:59:59.999Z")),
+                Future.future());
+
+        ordersAndFutures.keySet().stream()
+                .forEach(order -> mongoClient.insert(ORDERS, order, ordersAndFutures.get(order).completer()));
+
+        CompositeFuture.all(ordersAndFutures.values().stream().collect(Collectors.toList())).setHandler(event ->
+            vertx.createHttpClient().getNow(VERTICLE_PORT, "localhost", "/orders/month/2016/8/John%20Doe", response -> {
+                        testContext.assertTrue(response.statusCode() == 200);
+                        response.bodyHandler(body -> testContext.assertEquals(body.toString(), "579"));
+                        async.complete();
+                    })
         );
     }
 }
